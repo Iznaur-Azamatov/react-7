403da6ba42b2cdd1b686f1aac2854ca64c30f8a1
Azamatov Iznaur <azamatoviznaur@gmail.com>
2024-01-11 02:29:10 +0300

    added README.md

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Heroes from './Heroes';
+
+function App () {
+  return (
+    <div>
+      <h1>My Heroes</h1>
+      <Heroes />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Heroes.jsx
@@ -0,0 +1,32 @@
+// Heroes.js
+import React, { useState } from 'react';
+
+const Heroes = () => {
+  const [names, setNames] = useState([
+    "Шерлок Холмс",
+    "Доктор Ватсон",
+    "Профессор Мориарти",
+    "Миссис Хадсон",
+    "Ирен Адлер"
+  ]);
+
+  const handleDelete = (index) => {
+    const updatedNames = names.filter((item, id) => id !== index);
+    setNames(updatedNames);
+  };
+
+  return (
+    <div>
+      <h2>Список героев:</h2>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index} onClick={() => handleDelete(index)}>
+            {name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Heroes;
